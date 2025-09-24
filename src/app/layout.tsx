51d7adb785bb5f80b6515e789492c663a6549cb3
Rohan Kale <rohan.kale6123@gmail.com>
2025-09-24 20:07:11 +0530

    Bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next"
 
 const mono = JetBrains_Mono({
@@ -30,15 +29,6 @@ export default function RootLayout({
                     }}
                 />
                 {children}
-                <Script type="text/javascript">
-                    {`
-          (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "tfrtz2ox02");
-          `}
-                </Script>
                 <Analytics />
             </body>
         </html>
